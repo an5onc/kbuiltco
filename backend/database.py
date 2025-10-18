6891e770c8f7b06dd3dbdc6111b8691ed3809b2e
InterlockGoEvans <cord3576@bears.unco.edu>
2025-10-18 14:28:25 -0600

    olkpo

--- backend/database.py
+++ backend/database.py
@@ -4,4 +4,5 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 SQLALCHEMY_DATABASE_URL = "sqlite:///./raffle.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=False)
 Base = declarative_base()
