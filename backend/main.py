7bb9ea94c824591d0fa0efee4a03c82e0def20c1
InterlockGoEvans <cord3576@bears.unco.edu>
2025-10-17 20:40:44 -0600

    CORS fix

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://an5onc.github.io"],
+    allow_origins=["https://an5onc.github.io", "https://an5onc.github.io/kbuiltco"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
